9cd4f6b12be6cbd3dc8448179de21d726036a2d1
xcl <2870175885@qq.com>
2019-07-05 20:57:22 +0800

    完善的投票系统

--- mydemo/myvote/views.py
+++ mydemo/myvote/views.py
@@ -13,24 +13,25 @@ from .models import Questions,Choice
 
 
 def index(request):
-    questions = Questions.objects.all()
-    return render(request, "myvote/index.html",{"questions":questions})
+    return render(request, "myvote/index.html")
 
 def questionlist(request):
     questions = Questions.objects.all()
     return render(request, "myvote/questionlist.html", {"questions": questions})
 
 def choicelist(request,id):
+    question = Questions.objects.get(pk=id)
     if request.method == "GET":
-        questions = Questions.objects.get(pk=id)
-        return render(request, "myvote/choicelist.html", {"questions": questions})
+
+        return render(request, "myvote/choicelist.html", {"question": question})
     elif request.method == "POST":
         cid = request.POST.get("choice")
         choice = Choice.objects.get(pk=cid)
         choice.votesnum += 1
         choice.save()
+        # 重定向
         return redirect(reverse("myvote:choiceresult", args=(id,)))
 
 def choiceresult(request,id):
-    questions = Questions.objects.get(pk=id)
-    return render(request, "myvote/choiceresult.html", {"questions": questions})
+    question = Questions.objects.get(pk=id)
+    return render(request, "myvote/choiceresult.html", {"question": question})
